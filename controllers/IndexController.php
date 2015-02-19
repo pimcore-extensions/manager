9a5f33a7d4cbbea53b19e8937c3b7db5820bee30
Rafal Galka <rafal.galka@modernweb.pl>
2015-02-19 17:53:00 +0100

    Check if project's composer.json is writable

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -2,14 +2,14 @@
 
 
 class Manager_IndexController extends Pimcore_Controller_Action_Admin {
-    
+
     public function indexAction () {
 
         $client = new Packagist\Api\Client();
 
         $packagistResults =  $client->search("", array("type" => "pimcore-plugin"));
         $results = array();
-        
+
         foreach($packagistResults as $result)
         {
             $results[] = array(
@@ -21,18 +21,18 @@ class Manager_IndexController extends Pimcore_Controller_Action_Admin {
                 "repository" => $result->getRepository()
             );
         }
-        
+
         $this->_helper->json(array("success" => true, "packages" => $results));
     }
-    
+
     public function installAction()
     {
         $name = $this->getParam("name", null);
         $client = new Packagist\Api\Client();
-        
+
         if(!$name)
             $this->_helper->json(array("success" => false, "message" => "no package name supplied"));
-        
+
         try
         {
             $package = $client->get($name);
@@ -41,42 +41,49 @@ class Manager_IndexController extends Pimcore_Controller_Action_Admin {
         {
             return $this->_helper->json(array("success" => false, "message" => "packagist package with name '$name' not found"));
         }
-        
+
         $versions = $package->getVersions();
-        
+
         foreach($versions as $version=>$infos)
         {
             if($version == "dev-master")
                 continue;
-            
+
             $version = $versions[$version];
             break;
         }
-        
+
         if(!$version instanceof Packagist\Api\Result\Package\Version)
             $version = $versions['dev-master'];
-        
+
         if(!$version instanceof Packagist\Api\Result\Package\Version)
             return $this->_helper->json(array("success" => false, "message" => "no version for package with name '$name' not found"));
-            
-            
+
+
         $config = Manager_Composer::getComposerConfiguration();
-        
+
         if(!$config)
             $this->_helper->json(array("success" => false, "message" => "no composer json found"));
-            
+
         if(!is_array($config['require']))
             $config['require'] = array();
-        
+
         $config['require'][$name] = $version->getVersion();
-        
-        if(Manager_Composer::writeComposerConfiguration($config))
-        {
-           Manager_Composer::update();
-            
-            $this->_helper->json(array("success" => true));
+
+        if(!Manager_Composer::writeComposerConfiguration($config)) {
+            return $this->_helper->json(array(
+                'success' => false,
+                'message' => 'composer.json is not writable, please check permissions'));
         }
-        else
-            return $this->_helper->json(array("success" => false, "message" => "couldn't write composer json"));
+
+        try {
+            Manager_Composer::update();
+        } catch (Exception $e) {
+            return $this->_helper->json(array(
+                'success' => false,
+                'message' => $e->getMessage()));
+        }
+
+        return $this->_helper->json(array("success" => true));
     }
 }
--- lib/Manager/Composer.php
+++ lib/Manager/Composer.php
@@ -6,47 +6,47 @@ class Manager_Composer
     public static function getComposerFile()
     {
         $composerFile = PIMCORE_DOCUMENT_ROOT . "/composer.json";
-        
+
         if(!is_file($composerFile))
             $composerFile = PIMCORE_DOCUMENT_ROOT . "/../composer.json";
-        
+
         if(is_file($composerFile))
             return $composerFile;
         return false;
     }
-    
+
     public static function getComposerConfiguration()
     {
         $file = self::getComposerFile();
-        
+
         if($file)
             return json_decode(file_get_contents($file), true);
-        
+
         return false;
     }
-    
+
     public static function writeComposerConfiguration($config)
     {
         $file = self::getComposerFile();
-        
-        if($file)
+
+        if ($file && is_writable($file))
             return file_put_contents($file, json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-        
+
         return false;
     }
-    
+
     public static function update()
     {
         // change out of the webroot so that the vendors file is not created in
         // a place that will be visible to the intahwebz
         chdir(PIMCORE_DOCUMENT_ROOT);
-        
+
         putenv('COMPOSER_HOME=' . PIMCORE_DOCUMENT_ROOT . '/vendor/composer/composer/bin/composer');
-        
+
         //Create the commands
         $input = new Symfony\Component\Console\Input\ArrayInput(array('command' => 'update'));
         //$output = new Symfony\Component\Console\Output\StreamOutput(fopen('php://output','w'));
-        
+
         //Create the application and run it with the commands
         $application = new Composer\Console\Application();
         $application->setAutoExit(false);
